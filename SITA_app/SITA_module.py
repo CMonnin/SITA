6466ac2ed37e86fe8d696136112284cdfd3a26c9
Cian Monnin <cianmonnin@gmail.com>
2023-11-03 17:43:29 -0400

    small changes

--- SITA_app/SITA_module.py
+++ SITA_app/SITA_module.py
@@ -2,6 +2,7 @@ import logging
 import re
 import sys
 from collections import Counter
+import itertools
 
 import numpy as np
 
@@ -30,19 +31,27 @@ logger.addHandler(file_handler)
 # MDVa = Experimentally measured Mass distrubtion vector normalised to 1
 
 
-class Labelled_compound:
-    # dict of the natural abundance of common elements
-    # source: XXXXX
-    ISOTOPE_ABUNDANCE_DICT_UNIT_MASS = {
-        "H": {"abundance": (0.999885, 0.000115), "mass": (1, 2)},
-        "C": {"abundance": (0.9893, 0.0107), "mass": (12, 13)},
-        "N": {"abundance": (0.99632, 0.00368), "mass": (14, 15)},
-        "O": {"abundance": (0.99757, 0.00038, 0.00205), "mass": (16, 17, 18)},
-        "Si": {"abundance": (0.922297, 0.046832, 0.030872), "mass": (28, 29, 30)},
-        "P": {"abundance": (1), "mass": (31)},
-        "S": {"abundance": (0.9493, 0.0076, 0.0429, 0.0002), "mass": (32, 33, 34, 36)},
-    }
+# dict of the natural abundance of common elements
+# Source:
+# Coursey, J. S., et al. "Atomic weights and isotopic compositions with
+# relative atomic masses." NIST Physical Measurement Laboratory (2015).
+# https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl
 
+# TODO adducts Ca, Na, NH4, FA, AA, Li,
+ISOTOPE_ABUNDANCE_DICT_UNIT_MASS = {
+    "H": {"abundance": (0.999885, 0.000115), "mass": (1, 2)},
+    "C": {"abundance": (0.9893, 0.0107), "mass": (12, 13)},
+    "N": {"abundance": (0.99636, 0.00364), "mass": (14, 15)},
+    "O": {"abundance": (0.99757, 0.00038, 0.00205), "mass": (16, 17, 18)},
+    "Si": {"abundance": (0.92223, 0.04685, 0.03092), "mass": (28, 29, 30)},
+    "P": {"abundance": (1), "mass": (31)},
+    "S": {"abundance": (0.9499, 0.0075, 0.0425, 0.0001), "mass": (32, 33, 34, 36)},
+    "Cl": {"abundance": (0.7576, 0.2424), "mass": (35, 37)},
+    "Br": {"abundance": (0.5069, 0.4931), "mass": (79, 81)},
+}
+
+
+class Labelled_compound:
     def __init__(self, formula, labelled_element):
         """
         Each compound will have a molecular formula and
@@ -75,14 +84,56 @@ class Labelled_compound:
         logger.debug(elements)
         return elements
 
-    def correction_matrix(atom_ID, number_of_atoms):
+    def matrix_populator(self, isotope_ID: str, number_of_atoms: int):
+        """this will take an istope and generate all the possible combinations
+        that can occur depending on the number of atoms present in the compound
+        practically only a subset will be used to populate the matrix which is
+        dependant on the size of the matrix
+        this might have to be changed. I don't know how much of performance hit
+        it will cause.
+        """
+
+        # getting the mass of the 1st element of "mass" which is the most
+        # abundant
+        base_mass = (
+            ISOTOPE_ABUNDANCE_DICT_UNIT_MASS[isotope_ID].get("mass")[0]
+            * number_of_atoms
+        )
+        number_of_isotopes = len(
+            ISOTOPE_ABUNDANCE_DICT_UNIT_MASS[isotope_ID]["abundance"]
+        )
+        mass_list = []
+        current_mass = base_mass
+        for i in range(0, number_of_atoms + 1):
+            mass_list.append(current_mass)
+            current_mass += 1
+
+        combinations = itertools.combinations_with_replacement(
+            range(number_of_isotopes), number_of_atoms
+        )
+
+    def correction_matrix(
+        atom_ID: str, experimental_peak_number: int, number_of_atoms: int | None
+    ):
         """
         calculates a matrix for one element
 
         parameters
-        atom_ID: what the atom is
-        number_of_atoms: the number of the selected atom present in the molecular formula
+        ----------
 
+        atom_ID: str
+            what the atom is
+        experimental_peak_number: int
+            takes the number of peaks seen in the experimental data and creates a matrix to match the size
+        number_of_atoms: int
+            the number of the selected atom present in the molecular formula
+        returns
+        -------
+        the correction matrix
+
+
+        notes
+        -----
         eg for a 4x4 matrix for C, it's calculated out to M+3
 
 
@@ -100,9 +151,9 @@ class Labelled_compound:
         # right now calculated out to n+1, which is the maximum number of peaks that would be seen
         # in practice this probably only needs to go out to M+5 or M+6
 
-        corr_matrix = np.zeros(number_of_atoms + 1, number_of_atoms + 1)
+        corr_matrix = np.zeros(experimental_peak_number, experimental_peak_number)
         xxxx = 1
-        for i in range(number_of_atoms + 1):
+        for i in range(experimental_peak_number):
             for j in range(i + 1):
                 if i < j:
                     corr_matrix[i, j] = 0
@@ -140,7 +191,7 @@ def correction_matrix_test(
     Si_num=0,
     S_num=0,
 ):
-    """that matrix needs to take into accont the number of
+    """that matrix needs to take into account the number of
     units the user wants to calculate out to. Use itertools
     to calculate the the different combinations that are required.
     eg O has 16O, 17O, and 18O that can contribute.
@@ -158,8 +209,4 @@ def correction_matrix_test(
         for j in range(size_of_matrix):
             matrix[i, j] = XXXXXX
 
-    pass
-
-
-def matrix_creator(element, size_of_matrix):
     pass
